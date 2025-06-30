3ac45b4c250da82baf613343503c590edc270e36
Roshini <roshinitavva@gmail.com>
2025-06-30 18:26:35 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      document.querySelector(link.getAttribute('href'))
-              .scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-  
